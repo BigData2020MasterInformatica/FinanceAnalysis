d1f0e8f8456949838f60ec9bde9f2e0999cdbe34
ajnebro <ajnebro@users.noreply.github.com>
2020-10-27 20:15:16 +0100

    Modify file setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 from setuptools import setup
 
+
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
@@ -8,14 +9,15 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name = "finanSP",
-    version = "0.0.1",
-    author = "Big Data subject team",
-    author_email = "ajnebro@uma.es",
-    description = ("Project to analyze finance data with Apache Spark"),
-    license = "MIT",
-    keywords = "Spark Finance Streaming Clustering Classification",
+    name="finanSP",
+    version="0.0.1",
+    author="Big Data subject team",
+    author_email="ajnebro@uma.es",
+    description=("Project to analyze finance data with Apache Spark"),
+    license="MIT",
+    keywords="Spark Finance Streaming Clustering Classification",
     packages=['finansp'],
     long_description=read('README.rst'),
     classifiers=[
